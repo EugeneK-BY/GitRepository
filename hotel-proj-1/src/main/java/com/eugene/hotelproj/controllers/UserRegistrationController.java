57fd6c7dc4d7c1294caed36c2eb8716986aa941c
EugeneK-BY <konyshkoen@tut.by>
2014-05-04 21:46:45 +0300

    user registration page

--- hotel-proj-1/src/main/java/com/eugene/hotelproj/controllers/UserRegistrationController.java
+++ hotel-proj-1/src/main/java/com/eugene/hotelproj/controllers/UserRegistrationController.java
@@ -1,22 +1,31 @@
 package com.eugene.hotelproj.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.eugene.hotelproj.models.HotelUser;
+import com.eugene.hotelproj.models.UserProfile;
+import com.eugene.hotelproj.models.UserRole;
 
 @Controller
 public class UserRegistrationController {
 
 	
-	@RequestMapping(value="userRegistration", method=RequestMethod.POST, produces = "text/html" )
-	public ModelAndView createNewUser(){
-		ModelAndView modelAndView =  new ModelAndView("userRegistration");
-		
-		modelAndView.addObject("header", "Регистрация пользователя");
-		
-		
+	@RequestMapping(value="/registration/{userType}", produces = "text/html" )
+	public ModelAndView createNewUser(@PathVariable("userType") String userType){
+		ModelAndView modelAndView =  new ModelAndView("registration/userRegistration");
+		if(userType.equals("user")){
+			modelAndView.addObject("page_header", "Регистрация пользователя");
+			modelAndView.addObject("hotelUser", new HotelUser());
+		}else if(userType.equals("employee")){
+			modelAndView.addObject("page_header", "Регистрация сотрудника");
+		}else{
+			modelAndView.addObject("page_header", "Регистрация");
+		}
 		return modelAndView;
 	}
-	
+
 }
